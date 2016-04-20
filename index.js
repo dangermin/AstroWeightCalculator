5155b9a3d7174e468bbf2fbe594aaa8cc29968d7
dngrcode <dangerminfield@Dangermins-MacBook-Pro-2.local>
2016-04-19 21:13:31 -0700

    First commit, could improve theAnswer function

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+function theAnswer(){
+	var x = document.getElementById("weight").value;
+	var y = document.getElementById("planets").value;
+	var z = x * y;
+	if (z != NaN ){
+		document.getElementById("answerField").innerHTML="Your weight is " + Math.round(z) + " space pounds";
+		}
+		else {
+	  	document.getElementById("answerField").innerHTML="Not a valid input";
+		}
+	}
